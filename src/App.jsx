49b8a0e599b85ebf7fa5675f3e27f4b8c73e0bbf
Rishab <rishab.chandgothia.cg@gmail.com>
2026-04-11 17:42:09 +0530

    fixed the vercel issue

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import LoadingScreen from './components/LoadingScreen';
 import Cursor from './components/Cursor';
 import AOS from 'aos';
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import 'aos/dist/aos.css';
 import { Toaster } from 'react-hot-toast';
 import { motion } from 'framer-motion';
